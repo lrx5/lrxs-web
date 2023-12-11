fb9ec9c7187fd50f6813ba3cdf64fcca7b596b68
Lraxious <eghi.nolasco@gmail.com>
2023-12-11 15:21:01 +0800

    test: import as usual

---
+++ src/components/Projects.tsx
@@ -0,0 +1,58 @@
+import { Link } from 'react-router-dom';
+import '../Projects.css';
+import karzhyn_T from './assets/karzhyn_t.png';
+import friends_T from './assets/friendslistv_t.png';
+import memory_T from './assets/memory_t.png';
+import caster_T from './assets/casterwheel.png';
+import salad_T from './assets/salad_t.jpg';
+import king_T from './assets/kingtiger_t.png';
+import start_T from './assets/startup_t.png';
+import mystery_T from './assets/mw_t.jpg';
+import cafe_T from './assets/cafeight_t.png';
+import rx7_T from './assets/rx7_t.png';
+import ingen_T from './assets/ingen_t.png';
+interface Project {
+  id: number;
+  file: string;
+  title: string;
+  description: string;
+  img: string;
+}
+
+const Projects: React.FC = () => {
+  // Dummy data for project cards
+  const projects: Project[] = [
+    { id: 11, file: 'karzhyn-website', title: 'Karzhyn Website', description: 'Bootstrap', img:karzhyn_T},
+    { id: 10, file: 'friends-list-website', title: 'Friends List Website', description: 'Vanilla HTML', img:friends_T},
+    { id: 9, file: 'create-with-clint-double-memory', title: 'Memory', description: 'Blender', img:memory_T},
+    { id: 8, file: 'caster-wheel', title: 'Caster Wheel', description: 'Blender', img:caster_T},
+    { id: 7, file: 'salad-kitchen', title: 'Salad Kitchen', description: 'Blender', img:salad_T},
+    { id: 6, file: 'king-tiger', title: 'King Tiger', description: 'Blender', img:king_T},
+    { id: 5, file: 'start-up-themed-backgrounds', title: '"Start Up" Themed backgrounds', description: 'Blender', img:start_T},
+    { id: 4, file: 'mystery-word', title: 'Mystery Word: Five Letters', description: 'Unity', img:mystery_T},
+    { id: 3, file: 'cafeight', title: 'CafEight', description: 'Blender', img:cafe_T},
+    { id: 2, file: 'rx7-fd', title: 'RX-7 Model', description: 'Blender', img:rx7_T},
+    { id: 1, file: 'ingen', title: 'Ingen', description: 'Blender', img:ingen_T},
+    // Add more projects as needed
+  ];
+
+  return (
+    <div className="projects-grid">
+      {projects.map((project, index) => (
+        <Link key={project.id} to={`/lrxs-web/${project.file}`}>
+          <div className="project-card" style={{ animationDelay: `${index * 0.2}s` }}>
+            <div>
+              <img src={project.img} alt="" />
+            </div>
+            <div>
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop.tsx';
+import Nav from './components/Nav.tsx';
+import HomePage from './components/HomePage.tsx';
+import About from './components/About.tsx';
+import Contact from './components/Contact.tsx';
+import KarzhynWebsite from './components/KarzhynWebsite.tsx';
+import FriendsList from './components/FriendsList.tsx';
+import Memory from './components/Memory.tsx';
+import Caster from './components/Caster.tsx';
+import Salad from './components/Salad.tsx';
+import NoProject from './components/NoProject.tsx';
+import KingTiger from './components/KingTiger.tsx';
+import StartUp from './components/StartUp.tsx';
+import CafEight from './components/CafEight.tsx';
+import MysteryWord from './components/MysteryWord.tsx';
+import Rx7 from './components/Rx7.tsx';
+import Ingen from './components/Ingen.tsx';
+import './index.css';
+import './Projects.css';
+import './ProjectsBlog.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ScrollToTop />
+      <Nav />
+      <Routes>
+        <Route path="/lrxs-web/" element={<HomePage />} />
+        <Route path="/lrxs-web/about" element={<About />} />
+        <Route path="/lrxs-web/contact" element={<Contact />} />
+        <Route path="/lrxs-web/karzhyn-website" element={<KarzhynWebsite />} />
+        <Route path="/lrxs-web/friends-list-website" element={<FriendsList />} />
+        <Route path="/lrxs-web/create-with-clint-double-memory" element={<Memory />} />
+        <Route path="/lrxs-web/caster-wheel" element={<Caster />} />
+        <Route path="/lrxs-web/salad-kitchen" element={<Salad />} />
+        <Route path="/lrxs-web/king-tiger" element={<KingTiger />} />
+        <Route path="/lrxs-web/start-up-themed-backgrounds" element={<StartUp />} />
+        <Route path="/lrxs-web/cafeight" element={<CafEight />} />
+        <Route path="/lrxs-web/mystery-word" element={<MysteryWord />} />
+        <Route path="/lrxs-web/rx7-fd" element={<Rx7 />} />
+        <Route path="/lrxs-web/ingen" element={<Ingen />} />
+        <Route path="/lrxs-web/:NoProject" element={<NoProject />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
+
